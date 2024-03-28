6c6dcc4c17ac789205934272010f87ba1f1d66bf
Ilyasj01 <151402471+Ilyasj01@users.noreply.github.com>
2024-03-28 12:22:32 +0000

    Update AjouterProduitt.java

--- EShop 3.0/java/produit/AjouterProduitt.java
+++ EShop 3.0/java/produit/AjouterProduitt.java
@@ -1,1 +1,31 @@
-package produit;import classes.BD;import classes.Produit;import java.io.IOException;import javax.servlet.RequestDispatcher;import javax.servlet.ServletException;import javax.servlet.http.HttpServlet;import javax.servlet.http.HttpServletRequest;import javax.servlet.http.HttpServletResponse;public class AjouterProduitt extends HttpServlet {    private static final long serialVersionUID = 1L;    public AjouterProduitt() {    }    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {        String quantite = request.getParameter("qte");        String description = request.getParameter("desc");        String designation = request.getParameter("desi");        BD.addProduit(new Produit(description, Integer.valueOf(quantite), designation));        RequestDispatcher dispatch = request.getRequestDispatcher("lister.jsp");        dispatch.forward(request, response);    }}
+package produit;
+
+import classes.BD;
+import classes.Produit;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjouterProduitt extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  public AjouterProduitt() {}
+
+  protected void doPost(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    String quantite = request.getParameter("qte");
+    String description = request.getParameter("desc");
+    String designation = request.getParameter("desi");
+    BD.addProduit(
+      new Produit(description, Integer.valueOf(quantite), designation)
+    );
+    RequestDispatcher dispatch = request.getRequestDispatcher("lister.jsp");
+    dispatch.forward(request, response);
+  }
+}
