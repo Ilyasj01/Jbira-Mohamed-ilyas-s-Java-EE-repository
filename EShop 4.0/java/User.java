fd20f964309d3b0edf4b3774433324917e41f53e
Ilyasj01 <151402471+Ilyasj01@users.noreply.github.com>
2024-03-28 12:24:30 +0000

    Update User.java

--- EShop 4.0/java/User.java
+++ EShop 4.0/java/User.java
@@ -1,1 +1,75 @@
-import java.io.Serializable;public class User implements Serializable {    private static final long serialVersionUID = 1L;    private int id;    private String username;    private String pwd;    private String lastname;    private String firstname;    public User() {    }    public User(int id, String username, String pwd, String lastname, String firstname) {        this.id = id;        this.username = username;        this.pwd = pwd;        this.lastname = lastname;        this.firstname = firstname;    }    public int getId() {        return this.id;    }    public void setId(int id) {        this.id = id;    }    public String getNom() {        return this.username;    }    public void setNom(String nom) {        this.username = nom;    }    public String getPwd() {        return this.pwd;    }    public void setPwd(String pwd) {        this.pwd = pwd;    }    public String getLastname() {        return this.lastname;    }    public void setLastname(String lastname) {        this.lastname = lastname;    }    public String getFirstname() {        return this.firstname;    }    public String getUsername() {        return this.username;    }    public void setUsername(String username) {        this.username = username;    }    public void setFirstname(String firstname) {        this.firstname = firstname;    }}
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  private int id;
+  private String username;
+  private String pwd;
+  private String lastname;
+  private String firstname;
+
+  public User() {}
+
+  public User(
+    int id,
+    String username,
+    String pwd,
+    String lastname,
+    String firstname
+  ) {
+    this.id = id;
+    this.username = username;
+    this.pwd = pwd;
+    this.lastname = lastname;
+    this.firstname = firstname;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNom() {
+    return this.username;
+  }
+
+  public void setNom(String nom) {
+    this.username = nom;
+  }
+
+  public String getPwd() {
+    return this.pwd;
+  }
+
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
+
+  public String getLastname() {
+    return this.lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getFirstname() {
+    return this.firstname;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+}
